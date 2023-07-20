5638d5182923bcd5599d6b5dde4f7c455a110349
Ekansh <ekanshbhushan2k22@gmail.com>
2023-07-20 21:53:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Header from './Header';
+
+
+function App() {
+  return (
+    <div className='app_wrapper'>
+      {/* <Header/> */}
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='header__left'>
+            <div className='header__logo'>
+                <img src='./skillop only logo.png'/>
+            </div>
+            <div className='header__search'>
+            
+            </div>
+        </div>
+
+        <div className='header__right'>
+            
+        </div>
+    </div>
+  )
+}
