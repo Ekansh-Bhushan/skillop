9a32d7ac2c4c2b7341ae8726dd809459de7c19b4
anuragak021 <114359604+anuragak021@users.noreply.github.com>
2023-07-25 21:18:48 +0530

    New Files added

--- src/SlotBooking.js
+++ src/SlotBooking.js
@@ -2,20 +2,22 @@ import React from 'react'
 import "./css/Login.css"
 import img1 from './img/skillop.png'
 
-function Slot() {
+function SlotBooking() {
+
+
   return (
+
     <div className='loginScreen'>
       <div>
       <img src={img1} alt="skillop" />
       </div>
       <form >
-        
+        <input required type='text' placeholder='Name Of the Mentor' ></input>
         <input required type='text' placeholder='Enter the Day' ></input>
         <input required type='text' placeholder='Enter the Time Slot' ></input>
 
         <h4>Note: Please carefully Enter the detials</h4>
-        <h4>Note: If there are more than one day than please put comma (,)</h4>
-        <h4>Note: please put time slots like 10-11 am/pm</h4>
+        <h4>Changes Won't be possible after the successful payment</h4>
         <input type='submit' placeholder='Continue'></input>
 
       </form>
@@ -24,4 +26,4 @@ function Slot() {
   )
 }
 
-export default Slot
+export default SlotBooking
--- src/profile.js
+++ src/profile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./css/profile.css"
+import "./css/.css"
 
 function Profile() {
     <>
@@ -11,52 +11,117 @@ function Profile() {
     <link rel="stylesheet" href="styles.css"/>
     </>
   return (
-    <main>
+    <main class ="main">
     <header>
-        <nav>
-            <div class = "left">Ekansh</div>
-            <div class = "right">
-                <ul>
-                    <li><a href="#home"  class="active">Home</a></li>
-                    <li><a href="#about" >About</a></li>
-                    <li><a href="#services" >Services</a></li>
-                    <li><a href="#skills" >Projects</a></li>
-                    <li><a href="#contact" >Contact Me</a></li>
-                </ul>
-            </div>
-        </nav>
+      <nav>
+        <div class="left">Ekansh</div>
+        <div class="right">
+          <ul>
+            <li><a href="#home" class="active">Home</a></li>
+            <li><a href="#about">My Keypoints</a></li>
+  
+            <li><a href="#contact">Slots</a></li>
+          </ul>
+        </div>
+      </nav>
     </header>
 
     <section class="firstSection">
-        <div class="leftSection">
-            <h5>HI, My name is </h5>
-            <div class="purple"><h1 id="slide">Ekansh</h1></div>
-            
-            <h5>and I am a passionate</h5>
-            
-            <span id="element"></span>
-            
-            <p>I am 1<sup>st</sup> year student in DTU</p>
-            <div class="home-sci">
-                <a href="https://www.instagram.com/ekansh__bhushan/" target="_blank"><i class='bx bxl-instagram'></i></a>
-                <a href="https://github.com/Ekansh-Bhushan" target="_blank"><i class='bx bxl-github' ></i></a>
-                <a href="#"><i class='bx bxl-whatsapp' ></i></a>
-                <a href="#" ><i class='bx bxl-linkedin' ></i></a>
-                <a href="https://www.youtube.com/@ekansh4518/featured" target="_blank" ><i class='bx bxl-youtube' ></i></a>
-            </div>
-            <a href="#" class="btn-box">More About Me</a>
+      <div class="leftSection">
+        <h5>HI, My name is</h5>
+        <div class="purple"><h1 id="slide">Ekansh</h1></div>
+        {/* user.name */}
+        <h5>and I am a passionate</h5>
+        <span id="element"></span>
+        <p>I am 1<sup>st</sup> year student in DTU</p>
+        {/* here will be the info of the person */}
+        {/* <div class="home-sci">
+          <a href="#" ><i class='bx bxl-linkedin' ></i></a>
+        </div> */}
+        <a href="#" class="btn-box">Linkedin</a>
+      </div>
+      <div class="rightSection">
+        <div class="box">
+          <div class="content">
+            <img src="https://pps.whatsapp.net/v/t61.24694-24/358220047_3539108243029399_3321016758607747344_n.jpg?ccb=11-4&oh=01_AdTeAUrKyHo28hh4cSpFL2V_vzEpCKgrfrmFbWtexvtT7w&oe=64CBC61A" class="dp" alt="" />
+          </div>
         </div>
-        <div class="rightSection">
-            <div class="box">
-                <div class="content">
-                    {/* <img src="photo.jpg" class="dp" alt=""/> */}
-                </div>
-        
-            </div>
-            
-        </div>
+      </div>
     </section>
-    </main>
+
+
+    <section>
+      <div class="services" id="services">
+          <div class="container">
+              <h1 class="sub-title">My <span>Keypoints</span></h1>
+              <div class="services-list">
+                  <div>
+ 
+                      <h2>About Me</h2>
+                      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis qui error omnis, exercitationem dolor non nostrum asperiores odio sed mollitia. Beatae nisi tempore voluptatibus obcaecati temporibus, cumque 
+                          incidunt quisquam a! Dicta facilis consequuntur animi atque doloremque. Laboriosam laudantium eveniet facilis explicabo.</p>
+     
+                  </div>
+                  <div>
+      
+                      <h2>Education</h2>
+                      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis qui error omnis, exercitationem dolor non nostrum asperiores odio sed mollitia. Beatae nisi tempore voluptatibus obcaecati temporibus, cumque 
+                          incidunt quisquam a! Dicta facilis consequuntur animi atque doloremque. Laboriosam laudantium eveniet facilis explicabo.</p>
+                    
+                  </div>
+                  <div>
+        
+                      <h2>Experience</h2>
+                      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis qui error omnis, exercitationem dolor non nostrum asperiores odio sed mollitia. Beatae nisi tempore voluptatibus obcaecati temporibus, cumque 
+                          incidunt quisquam a! Dicta facilis consequuntur animi atque doloremque. Laboriosam laudantium eveniet facilis explicabo.</p>
+                    
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+
+  <section>
+      <div class="services" id="services">
+          <div class="container">
+              <h1 class="sub-title"> <span></span></h1>
+              <div class="services-list">
+                  <div>
+ 
+                      <h2>Past Experience</h2>
+                      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis qui error omnis, exercitationem dolor non nostrum asperiores odio sed mollitia. Beatae nisi tempore voluptatibus obcaecati temporibus, cumque 
+                          incidunt quisquam a! Dicta facilis consequuntur animi atque doloremque. Laboriosam laudantium eveniet facilis explicabo.</p>
+     
+                  </div>
+                  <div>
+      
+                      <h2>Future Plans</h2>
+                      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis qui error omnis, exercitationem dolor non nostrum asperiores odio sed mollitia. Beatae nisi tempore voluptatibus obcaecati temporibus, cumque 
+                          incidunt quisquam a! Dicta facilis consequuntur animi atque doloremque. Laboriosam laudantium eveniet facilis explicabo.</p>
+                    
+                  </div>
+                  <div>
+        
+                      <h2>Skills</h2>
+                      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis qui error omnis, exercitationem dolor non nostrum asperiores odio sed mollitia. Beatae nisi tempore voluptatibus obcaecati temporibus, cumque 
+                          incidunt quisquam a! Dicta facilis consequuntur animi atque doloremque. Laboriosam laudantium eveniet facilis explicabo.</p>
+                    
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <section>
+       <div class="services" id="services">
+          <h1 class="sub-title"> <span>Slots</span></h1>
+          <div class="services-list">
+              {/* slots will be added here */}
+          </div>
+      </div>
+  </section>
+  </main>
   )
 }
 
